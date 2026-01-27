728163947c300b728c8c913a5ed998fe73844f24
Cristóbal  Griffero <cristobal.griffero@gmail.com>
2026-01-27 13:04:30 -0300

    fix: Define transformer inline in initializer
    Move LowerCamelTransformer definition into the blueprinter
    initializer to avoid autoloading issues during Rails boot.

--- config/initializers/blueprinter.rb
+++ config/initializers/blueprinter.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+# Define the transformer class inline to avoid autoloading issues
+class LowerCamelTransformer < Blueprinter::Transformer
+  def transform(hash, _object, _options)
+    hash.transform_keys! { |key| key.to_s.camelize(:lower).to_sym }
+  end
+end
+
 Blueprinter.configure do |config|
   config.generator = Oj
   config.datetime_format = ->(datetime) { datetime&.iso8601 }
